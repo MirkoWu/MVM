b0d151adb4a30dcbfe26c57c372ff9be933c3827
MirkoWu <709651717@qq.com>
2021-08-13 01:07:06 +0800

    #Update load

--- lib_network/src/main/java/com/mirkowu/lib_network/load/OnProgressListener.java
+++ lib_network/src/main/java/com/mirkowu/lib_network/load/OnProgressListener.java
@@ -1,9 +1,5 @@
 package com.mirkowu.lib_network.load;
 
-import androidx.annotation.NonNull;
-
-import java.io.File;
-
 public interface OnProgressListener {
     void onProgress(long readBytes, long totalBytes);
 }
