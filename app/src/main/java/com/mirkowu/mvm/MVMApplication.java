3e4f7bf64e5068f27f748af761848b1c3b66d7a9
wumeng1 <1yw@Mirko>
2021-09-24 18:57:18 +0800

    #update 工具

--- app/src/main/java/com/mirkowu/mvm/MVMApplication.java
+++ app/src/main/java/com/mirkowu/mvm/MVMApplication.java
@@ -1,6 +1,7 @@
 package com.mirkowu.mvm;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 import com.mirkowu.lib_bugly.BuglyManager;
 import com.mirkowu.lib_crash.CrashManager;
@@ -53,4 +54,27 @@ public class MVMApplication extends Application {
         });
     }
 
+    /**
+     * 开启严苛模式，当代码有违规操作时，可以通过Logcat或崩溃的方式提醒我们
+     */
+    private void startStrictMode() {
+        if (BuildConfig.DEBUG) { //一定要在Debug模式下使用，避免在生产环境中发生不必要的崩溃和日志输出
+
+            //线程检测策略
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()  //检测主线程磁盘读取操作
+                    .detectDiskWrites() //检测主线程磁盘写入操作
+                    .detectNetwork() //检测主线程网络请求操作
+                    .penaltyLog() //违规操作以log形式输出
+                    .penaltyFlashScreen()
+                    .build());
+
+            //虚拟机检测策略
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects() //检测SqlLite泄漏
+                    .detectLeakedClosableObjects() //检测未关闭的closable对象泄漏
+                    .penaltyDeath() //发生违规操作时，直接崩溃
+                    .build());
+        }
+    }
 }
